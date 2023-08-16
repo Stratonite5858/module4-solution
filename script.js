53db5f621659dc00ec66cb010fdc6f05186a3fc9
Sean Collins <scollin10@gmail.com>
2023-08-16 12:10:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-var x = "Hello World!";
-console.log(x);
+(function () {
+
+  // Define the helloSpeaker object
+  var helloSpeaker = {};
+  helloSpeaker.speakWord = "Hello";
+  
+  helloSpeaker.speak = function (name) {
+    console.log(helloSpeaker.speakWord + " " + name);
+  };
+  
+  // Define the byeSpeaker object
+  var byeSpeaker = {};
+  byeSpeaker.speakWord = "Good Bye";
+  
+  byeSpeaker.speak = function (name) {
+    console.log(byeSpeaker.speakWord + " " + name);
+  };
+  
+  var names = ["Sean", "Caden", "Jho Ann", "Kim", "Kyong", "Kent", "Justin", "Kylie", "John", "Jackie"];
+  
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+  
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+  
+  })();
+  
